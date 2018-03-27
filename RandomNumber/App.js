ccfebaf1fc3f77aff5491c31be0929376c35d0c3
rafaelnascimentosilva <rafaelnasc.silv@gmail.com>
2018-03-27 13:12:18 -0300

    gerando números aleatórios

---
+++ RandomNumber/App.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Button 
+          title="Gerar número aleatório"
+          onPress={geraNumerosAleatorios}
+        />
+      </View>
+    );
+  }
+}
+
+const geraNumerosAleatorios = () => {
+  var numero_aleatorio = Math.random();
+  numero_aleatorio = Math.floor(numero_aleatorio * 10);
+  alert(numero_aleatorio);
+}
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
